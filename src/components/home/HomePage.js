916cc790abfdaf876f3bf1bb410f7bc94567ea9b
Kabeer Rifaye <citizen.kabeer@gmail.com>
2019-06-26 08:55:26 +0530

    Added Redux flow

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <div className="jumbotron">
+          <h1>Home Page</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Link to="table" className="btn btn-primary btn-lg">Learn More</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
